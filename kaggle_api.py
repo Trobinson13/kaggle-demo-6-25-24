c227a881b6fd7fd8b2251c666e4e7fd39e5c4738
Trobinson13 <trerrobinson@gmail.com>
2024-06-25 19:43:05 -0400

    added method to gather data via api

--- kaggle_api.py
+++ kaggle_api.py
@@ -1,6 +1,7 @@
 # run my imports
 import os
 from dotenv import load_dotenv
+import kaggle
 
 # load env variables
 load_dotenv()
@@ -10,3 +11,6 @@ os.environ['KAGGLE_USERNAME'] = os.getenv('KAGGLE_USERNAME')
 os.environ['KAGGLE_KEY'] = os.getenv('KAGGLE_KEY')
 
 # download the dataset from kaggle
+dataset = "claudiodavi/superhero-set"
+kaggle.api.dataset_download_files(dataset, path='.', unzip=True)
+
